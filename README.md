1a3d8cbfd2ee68e1feab0aa26f95ddecf5e9ba76
Matt Hernandez <matt@modulus.io>
2014-05-21 19:54:03 -0400

    Make it so 💅

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 sendgrid-example
 ================
 
-An example app that sends emails using Sendgrid.
+An example app that sends emails using [Sendgrid](https://sendgrid.com) on
+[Modulus](https://modulus.io).
+
+# Usage
+
+Create a new project, provision the Sendgrid add-on for your project, then
+deploy this application.
+
+    $ modulus project create sendgrid
+    $ modulus addons add sendgrid:free
+    $ modulus deploy
---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app     = express();
+
+var sendgrid = require('sendgrid')(
+  process.env.SENDGRID_USERNAME,
+  process.env.SENDGRID_PASSWORD
+);
+
+app.use(require('body-parser')());
+
+app.get('/', function (req, res) {
+  res.send([
+    '<form method="post" style="width:500px;" action="/">',
+    '<fieldset style="display:block;padding:5px;">',
+    '<label for="email" style="width:100px;">To:</label><br>',
+    '<input type="text" name="email" placeholder="text@example.com">',
+    '</fieldset>',
+    '<fieldset style="display:block;padding:5px;">',
+    '<label for="subject" style="width:100px;">Subject:</label><br>',
+    '<input type="text" name="subject" placeholder="Test Email">',
+    '</fieldset>',
+    '<fieldset style="display:block;padding:5px;">',
+    '<label for="message" style="width:100px;">Message:</label><br>',
+    '<textarea style="width:100%;height:80px;padding:2px;" name="message" placeholder="Whoa!"></textarea>',
+    '</fieldset>',
+    '<p><input type="submit" value="Send" /></p>',
+    '</form>'
+  ].join(''));
+});
+
+app.post('/', function (req, res) {
+  sendgrid.send({
+    to: req.body.email,
+    from: 'test@example.com',
+    subject: req.body.subject,
+    text: req.body.message
+  }, function (err, result) {
+      if (err) {
+        return res.send('Invalid format.');
+      }
+
+      res.send('Sent!');
+    });
+});
+
+app.listen(process.env.PORT || 3000);
